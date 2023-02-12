69577db15a160cf647346b37d5803e2fe052e926
Aman Negi <37607224+AmanNegi@users.noreply.github.com>
2023-02-12 17:33:53 +0530

    Update linked_list.cpp

--- linked_list.cpp
+++ linked_list.cpp
@@ -2,32 +2,38 @@
 
 using namespace std;
 
+// Create a Struct that defines the structure of each node
 struct Node
 {
 	int value;
 	Node *next;
 };
 
+// Define a class that will handle all the operations
 class LinkedList
 {
+// The methods and variables under private are not accessible outside the class
 private:
+	// Head node will be the pointer to the starting of linked list and should not be accessable from outside
 	Node *head;
 
 	Node *getNewNode(int v)
 	{
+		// Allocate space in memory using the new keyword
 		Node *newNode = new Node();
 		newNode->value = v;
 		newNode->next = NULL;
 	}
-
+// The functions under public can be used outside the class as well
 public:
 	int size = 0;
-	//* Default Constructor
+	
+	// Default Constructor
 	LinkedList()
 	{
 		head = NULL;
 	}
-	//* Parameterized Constructor
+	// Parameterized Constructor
 	LinkedList(int a)
 	{
 		head = getNewNode(a);
